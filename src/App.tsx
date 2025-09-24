d714980be556bc04fd631cc5d368f70e0ce075e6
Mohd-0706 <irfanmhd.0706@gmail.com>
2025-09-24 21:50:40 +0530

    Small changes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import PDFTools from './components/PDFTools';
 import ImageTools from './components/ImageTools';
-import About from './components/About';
 import Footer from './components/Footer';
 import { useDarkMode } from './hooks/useDarkMode';
 import { useActiveSection } from './hooks/useActiveSection';
@@ -39,7 +38,6 @@ function App() {
                 />
                 <PDFTools darkMode={darkMode} />
                 <ImageTools darkMode={darkMode} />
-                <About darkMode={darkMode} />
               </>
             } />
             
